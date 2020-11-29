69917805bfeb6b52677e4768edb8925f9c848a62
Felipe Marinho <felipmarinho@gmail.com>
2020-11-29 06:45:28 -0300

    feat ensure CarService know how to calculate final price

--- src/service/carService.js
+++ src/service/carService.js
@@ -1,8 +1,14 @@
+const Tax = require("../entities/tax");
 const BaseRepository = require("../repository/base/baseRepository");
 
 class CarService {
 	constructor({ cars }) {
 		this.carRepository = new BaseRepository({ file: cars })
+		this.taxesBasedOnAge = Tax.taxesBasedOnAge
+		this.currencyFormat = new Intl.NumberFormat('pt-br', {
+			style: 'currency',
+			currency: 'BRL'
+		})
 	}
 
 	getRandomPositionFromArray(list) {
@@ -23,6 +29,17 @@ class CarService {
 		return car
 	}
 
+	calculateFinalPrice(customer, carCategory, numberOfDays) {
+		const { age } = customer
+		const { price } = carCategory
+		const { then: tax } = this.taxesBasedOnAge.find(tax => age >= tax.from && age <= tax.to)
+
+		const finalPrice = ((tax * price) * (numberOfDays))
+
+		const formattedPrice = this.currencyFormat.format(finalPrice)
+
+		return formattedPrice
+	}
 }
 
 module.exports = CarService
